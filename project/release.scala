8b9ad88e3a8e13de244acc1f98d2a6d4b874d986
David Hoyt <dhoyt@hoytsoft.org>
2013-09-03 16:54:17 -0700

    Cleanup the SBT release code a bit.

--- project/release.scala
+++ project/release.scala
@@ -10,23 +10,27 @@ object ReleaseSettings {
   val defaults = Seq(
       //Customize the steps of the release process.
       releaseProcess := Seq[ReleaseStep](
-        checkSnapshotDependencies              //
-      , runTest                                //
-      , inquireVersions                        //
-      , setReleaseVersion                      //
-      , commitReleaseVersion                   // performs the initial git checks
-      , tagRelease                             //
-      , publishArtifacts                       //checks whether `publishTo` is properly set up
-      , setNextVersion                         //
-      , commitNextVersion                      //
-      , pushChanges                            //also checks that an upstream branch is properly configured
-    ),
+          checkSnapshotDependencies              //
+        , runTest                                //
+        , inquireVersions                        //
+        , setReleaseVersion                      //
+        , commitReleaseVersion                   //performs the initial git checks
+        , tagRelease                             //
+        , publishArtifacts                       //checks whether `publishTo` is properly set up
+        , setNextVersion                         //
+        , commitNextVersion                      //
+        , pushChanges                            //also checks that an upstream branch is properly configured
+      ),
+
       //Customize the next version string to bump the revision number.
-      nextVersion := { ver =>
-        Version(ver)
-          .map(_.bumpBugfix.asSnapshot.string)
-          .getOrElse(versionFormatError)
-      }
+      nextVersion := { ver => Version(ver).map(determineNextVersion(_)).getOrElse(versionFormatError) }
   ) ++ releaseSettings
+
+  def determineNextVersion(version: Version): String = {
+    version
+      .bumpBugfix
+      .asSnapshot
+      .string
+  }
 }
 
