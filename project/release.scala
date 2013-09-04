b51873579cd69460699003bfa6aec5d1bc586c23
David Hoyt <dhoyt@hoytsoft.org>
2013-09-03 17:16:12 -0700

    Try again.

--- project/release.scala
+++ project/release.scala
@@ -7,21 +7,7 @@ import ReleasePlugin._
 import ReleaseKeys._
 
 object ReleaseSettings {
-  val defaults = Seq(
-    //Customize the steps of the release process.
-    releaseProcess := Seq[ReleaseStep](
-        checkSnapshotDependencies              //
-      , runTest                                //
-      , inquireVersions                        //
-      , setReleaseVersion                      //
-      , commitReleaseVersion                   //performs the initial git checks
-      , tagRelease                             //
-      , publishArtifacts                       //checks whether `publishTo` is properly set up
-      , setNextVersion                         //
-      , commitNextVersion                      //
-      , pushChanges                            //also checks that an upstream branch is properly configured
-    )
-  ) ++ releaseSettings ++ Seq(
+  val defaults = releaseSettings ++ Seq(
     //Customize the next version string to bump the revision number.
     nextVersion := { ver => Version(ver).map(determineNextVersion(_)).getOrElse(versionFormatError) }
   )
