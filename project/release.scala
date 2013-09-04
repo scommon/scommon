508a02e682ddc2581303d34d95756e12fc550298
David Hoyt <dhoyt@hoytsoft.org>
2013-09-03 17:05:08 -0700

    Cleanup the release process. Bump the version by bug fix instead of minor.

--- project/release.scala
+++ project/release.scala
@@ -8,23 +8,23 @@ import ReleaseKeys._
 
 object ReleaseSettings {
   val defaults = Seq(
-      //Customize the steps of the release process.
-      releaseProcess := Seq[ReleaseStep](
-          checkSnapshotDependencies              //
-        , runTest                                //
-        , inquireVersions                        //
-        , setReleaseVersion                      //
-        , commitReleaseVersion                   //performs the initial git checks
-        , tagRelease                             //
-        , publishArtifacts                       //checks whether `publishTo` is properly set up
-        , setNextVersion                         //
-        , commitNextVersion                      //
-        , pushChanges                            //also checks that an upstream branch is properly configured
-      ),
-
-      //Customize the next version string to bump the revision number.
-      nextVersion := { ver => Version(ver).map(determineNextVersion(_)).getOrElse(versionFormatError) }
-  ) ++ releaseSettings
+    //Customize the steps of the release process.
+    releaseProcess := Seq[ReleaseStep](
+        checkSnapshotDependencies              //
+      , runTest                                //
+      , inquireVersions                        //
+      , setReleaseVersion                      //
+      , commitReleaseVersion                   //performs the initial git checks
+      , tagRelease                             //
+      , publishArtifacts                       //checks whether `publishTo` is properly set up
+      , setNextVersion                         //
+      , commitNextVersion                      //
+      , pushChanges                            //also checks that an upstream branch is properly configured
+    )
+  ) ++ releaseSettings ++ Seq(
+    //Customize the next version string to bump the revision number.
+    nextVersion := { ver => Version(ver).map(determineNextVersion(_)).getOrElse(versionFormatError) }
+  )
 
   def determineNextVersion(version: Version): String = {
     version
@@ -33,4 +33,3 @@ object ReleaseSettings {
       .string
   }
 }
-
